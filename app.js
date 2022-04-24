9e8513a972e5f4532438a5ad0641d75f11069daa
vilchezfelipe <felipemarket.vilchez@outlook.com>
2022-04-24 16:21:10 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,244 @@
+const objeto = [
+  {"nombre": "MINI PECHERA RENO", "codigo": "18011290", "imagen": "img/18011290.jpg"},
+  {"nombre": "CAPUCHON MONJE CAFE", "codigo": "18002222", "imagen": "img/18002222.jpg"},
+  {"nombre": "DECANTADOR RIEDEL CORNETO", "codigo": "18009824", "imagen": "img/18009824.jpg"},
+  {"nombre": "CYT AIREADOR VINOX BASIC", "codigo": "18011325", "imagen": "img/18011325.jpg"},
+  
+  {"nombre": "CYT CDD SOFTSHELL HOMBRE T/S", "codigo": "18006693", "imagen": "img/18006693.jpg"},
+  {"nombre": "CDD PARKA NEGRA HOMBRE TALLA L", "codigo": "18009404", "imagen": "img/18009404.jpg"},
+  {"nombre": "CDD POLERON DEVIL´S  TALLA M", "codigo": "18009403", "imagen": "img/18009403.jpg"},
+  {"nombre": "CDD POLERON DEVIL´S  TALLA XL", "codigo": "18009405", "imagen": "img/18009405.jpg"},
+  {"nombre": "CDD POLERON DEVIL´S  TALLA L", "codigo": "18009468", "imagen": "img/18009468.jpg"},
+  {"nombre": "CDD POLERON DEVIL´S  T/M", "codigo": "18009467", "imagen": "img/18009467.jpg"},
+  {"nombre": "MOCHTAL HOODY FLEECE PB", "codigo": "18009800", "imagen": "img/18009800.jpg"},
+  {"nombre": "CYT DUHKO SOFTSWEATHER MEN", "codigo": "18009798", "imagen": "img/18009798.jpg"},
+  {"nombre": "CYT CDD POLERA MU DOS DEVILS TALLA L", "codigo": "18010785", "imagen": "img/18010785.jpg"},
+  {"nombre": "CYT CDD POLERA MU DOS DEVILS TALLA XL", "codigo": "18010786", "imagen": "img/18010786.jpg"},  
+  {"nombre": "CYT MANUTD POLERA PIQUE NEGRA HOMBRE T/M", "codigo": "18006600", "imagen": "img/18006600.jpg"},
+  {"nombre": "POLERA PIQUE CDD BLANCO TALLA M", "codigo": "18002928", "imagen": "img/18002928.jpg"},
+    {"nombre": "CYT CDD POLERA PIQUE NEGRA HOMBRE T/S", "codigo": "18002591", "imagen": "img/18002591.jpg"},
+  {"nombre": "PARKA MANGA LARGA ACOLCHADA CYT", "codigo": "18008971", "imagen": "img/18008971.jpg"},
+    {"nombre": "PARKA SIN MANGA ACOLCHADA CYT", "codigo": "18008970", "imagen": "img/18008970.jpg"},
+    {"nombre": "CYT MANUTD POLAR NEGRO HOMBRE T/L", "codigo": "18007261", "imagen": "img/18007261.jpg"},
+
+    {"nombre": "CP POSAVASOS CUERINA C/ESCUDO CYT 1U CL", "codigo": "18003707", "imagen": "img/18003707.jpg"},
+
+  {"nombre": "SOMBRERO COLOR BARQUILLO", "codigo": "18010334", "imagen": "img/18010334.jpg"},
+  {"nombre": "SOMBRERO COLOR BARQUILLO", "codigo": "18006651", "imagen": "img/18006651.jpg"},
+  {"nombre": "SOMBRERO CONCHA Y TORO", "codigo": "18000034", "imagen": "img/18000034.jpg"},
+  {"nombre": "Delantal Jeans", "codigo": "18008985", "imagen": "img/18008985.jpg"},
+  {"nombre": "CP DELANTAL COCINA CUELLO RECTO CL", "codigo": "18004132", "imagen": "img/18004132.jpg"},
+  {"nombre": "PECHERA DE CUERO", "codigo": "18007801", "imagen": "img/18007801.jpg"},
+  
+  {"nombre": "PORTA BOTELLA CIRCULAR", "codigo": "18011289", "imagen": "img/18011289.jpg"},
+  {"nombre": "PORTA BOTELLA RECTANGULAR", "codigo": "18011288", "imagen": "img/18011288.jpg"},
+  {"nombre": "CP CJA MADERA CYT WINE SHOP 1BOT CL", "codigo": "13005808", "imagen": "img/13005808.jpg"},
+  
+  {"nombre": "TABLA GUILLI", "codigo": "18010330", "imagen": "img/18010330.jpg"},
+  {"nombre": "TABLA HACHI", "codigo": "18010331", "imagen": "img/18010331.jpg"},
+  {"nombre": "TABLA TEKUN", "codigo": "18010329", "imagen": "img/18010329.jpg"},
+  {"nombre": "TABLA QUESO + ACCESORIOS NP", "codigo": "18009863", "imagen": "img/18009863.jpg"},
+  {"nombre": "TABLAS QUESO CERAMICA NP", "codigo": "18009817", "imagen": "img/18009817.jpg"},
+  {"nombre": "DESCORCHADOR ECU ELECTRICO", "codigo": "18009738", "imagen": "img/18009738.jpg"},
+  {"nombre": "DESCORCHADOR DIABLO", "codigo": "18010171", "imagen": "img/18010171.jpg"},
+  {"nombre": "SACACORCHOS CYT MADERA", "codigo": "18009620", "imagen": "img/18009620.jpg"},
+  {"nombre": "CYT CDD SACACORCHOS NEGRO", "codigo": "18005922", "imagen": "img/18005922.jpg"},
+  {"nombre": "CP SACACORCHO CYT NUEVO 1U CL", "codigo": "18004103", "imagen": "img/18004103.jpg"},
+  {"nombre": "CDD SACACORCHOS NUEVO", "codigo": "18001941", "imagen": "img/18001941.jpg"},
+  {"nombre": "CYT SACACORCHO NEGRO C/LOGO", "codigo": "18000004", "imagen": "img/18000004.jpg"},
+
+  {"nombre": "TAZON CDD-MU 2018", "codigo": "18010147", "imagen": "img/18010147.jpg"},
+  {"nombre": "MUG CYT NARANJO", "codigo": "18009796", "imagen": "img/18009796.jpg"},
+  {"nombre": "BOTELLA ALUMINIO NP", "codigo": "18009864", "imagen": "img/18009864.jpg"},
+  {"nombre": "CYT DIABLO BOTELLA ACERO INOXIDABLE", "codigo": "18010989", "imagen": "img/18010989.jpg"},
+
+    {"nombre": "DESMANCHADOR VINO", "codigo": "18010333", "imagen": "img/18010333.jpg"},
+    {"nombre": "TAPON-ABRIDOR PARA ESPUMANTES PULLTEX", "codigo": "18010256", "imagen": "img/18010256.jpg"},
+    {"nombre": "TAPON ANTIOX PULLTEX", "codigo": "18010255", "imagen": "img/18010255.jpg"},
+    {"nombre": "TAPONES DE REPUESTO", "codigo": "18009435", "imagen": "img/18009435.jpg"},
+    {"nombre": "TAPON VINO SILICONA", "codigo": "18009433", "imagen": "img/18009433.jpg"},
+    
+    {"nombre": "CAPA DE AGUA", "codigo": "18010253", "imagen": "img/18010253.jpg"},
+    
+    
+    {"nombre": "LLAVERO METAL RECT-REDONDO NP", "codigo": "18009861", "imagen": "img/18009861.jpg"},
+    {"nombre": "LLAVERO METALICO CYT", "codigo": "18009023", "imagen": "img/18009023.jpg"},
+    {"nombre": "CP LLAVEROS CANASTO 1U CL", "codigo": "18003725", "imagen": "img/18003725.jpg"},
+    {"nombre": "CP LLAVERO CUERO - COBRE C-DIBUJO", "codigo": "18003512", "imagen": "img/18003512.jpg"},
+    {"nombre": "CYT CDD LLAVERO", "codigo": "18003408", "imagen": "img/18003408.jpg"},
+    {"nombre": "CAJA 1 BOT. BAMBU", "codigo": "18009745", "imagen": "img/18009745.jpg"},
+    {"nombre": "CP CAJA ECOCUERO 2BOT+ACCESORIOS CL", "codigo": "18006678", "imagen": "img/18006678.jpg"},
+    {"nombre": "CP CAJA ECOCUERO 1BOT+ACCESORIOS CL", "codigo": "18005578", "imagen": "img/18005578.jpg"},
+    {"nombre": "CP CAJA BAUL 6 BOTELLAS EXTENDIDO CYT CL", "codigo": "18005270", "imagen": "img/18005270.jpg"},
+
+    {"nombre": "IMAN PORTAL CYT", "codigo": "18009050", "imagen": "img/18009050.jpg"},
+    {"nombre": "IMAN CONCHA Y TORO", "codigo": "18008799", "imagen": "img/18008799.jpg"},
+
+    {"nombre": "CYT CDD HIELERA P21", "codigo": "18011176", "imagen": "img/18011176.jpg"},
+    {"nombre": "HIELERA ACRILICA NP", "codigo": "18009818", "imagen": "img/18009818.jpg"},
+    {"nombre": "CYT CDD HIELERA ROJA", "codigo": "18003948", "imagen": "img/18003948.jpg"},
+    {"nombre": "CDD HIELERA NEGRA C/LOGO DORADO", "codigo": "18009659", "imagen": "img/18009659.jpg"},
+    
+    
+    {"nombre": "SELFIE STICK NP", "codigo": "18009862", "imagen": "img/18009862.jpg"},
+    {"nombre": "LAPIZ METAL NP", "codigo": "18009815", "imagen": "img/18009815.jpg"},
+    {"nombre": "LAPIZ STAYLUS C Y T", "codigo": "18009378", "imagen": "img/18009378.jpg"},
+    {"nombre": "CDD LAPIZ 2016 PLATEADO", "codigo": "18009660", "imagen": "img/18009660.jpg"},
+    {"nombre": "LAPIZ CONCHA Y TORO", "codigo": "18008798", "imagen": "img/18008798.jpg"},
+    {"nombre": "CDD Lápiz 2016 Plateado", "codigo": "18009660", "imagen": "img/18009660.jpg"},
+    {"nombre": "BUFF CUELLO CYT NARANJO", "codigo": "18009797", "imagen": "img/18009797.jpg"},
+    
+   
+    
+
+    
+
+   
+    
+    {"nombre": "CDD STICKER ICONO DIABLO", "codigo": "18009412", "imagen": "img/18009412.jpg"},
+    {"nombre": "CDD STICKER LOGO MARCA", "codigo": "18009411", "imagen": "img/18009411.jpg"},
+    
+    {"nombre": "CYT CDD PENDRIVE NEGRO 16GB", "codigo": "18009244", "imagen": "img/18009244.jpg"},
+    
+    {"nombre": "PIN ESCUDO CYT", "codigo": "18009025", "imagen": "img/18009025.jpg"},
+    
+    
+    
+    
+    
+    {"nombre": "AIREADOR - DECANTADOR", "codigo": "18008517", "imagen": "img/18008517.jpg"},
+    {"nombre": "CP DECANT. AIREADOR DE VINO CENTELLINO", "codigo": "18008229", "imagen": "img/18008229.jpg"},
+    {"nombre": "CP JOCKEY BORDADO CYT AZUL/BEIGE 1U CL", "codigo": "18001669", "imagen": "img/18001669.jpg"},
+    {"nombre": "GORRO LEGIONARIO FILTRO UV CYT", "codigo": "18007982", "imagen": "img/18007982.jpg"},
+    {"nombre": "CP SET MANGA ENFRIADORA 1U CL", "codigo": "18003659", "imagen": "img/18003659.jpg"},
+    
+    {"nombre": "FUNDA NOTEBOOK CDD", "codigo": "18006699", "imagen": "img/18006699.jpg"},
+    {"nombre": "BOLSA CYT NEO 2 BOT. NARANJA", "codigo": "18009748", "imagen": "img/18009748.jpg"},
+    {"nombre": "CP FUNDA NEO BOT 750 CL", "codigo": "18004161", "imagen": "img/18004161.jpg"}, 
+    
+    
+    {"nombre": "CP CORTAGOTAS 1UN CON LOGO CYT CL", "codigo": "18006233", "imagen": "img/18006233.jpg"},
+    {"nombre": "CYT LIBRO CARMENERE ING", "codigo": "18006194", "imagen": "img/18006194.jpg"}, 
+    {"nombre": "CYT LIBRO CARMENERE ESP", "codigo": "18006193", "imagen": "img/18006193.jpg"}, 
+    
+    
+    {"nombre": "CDD PENDRIVE", "codigo": "18005523", "imagen": "img/18005523.jpg"},
+    {"nombre": "CYT CDD POLERA NEGRA POLO HOMBRE T/S", "codigo": "18005418", "imagen": "img/18005418.jpg"}, 
+     
+    {"nombre": "CP POLAR M/LARGA BLOCK FLEESE CL", "codigo": "18004250", "imagen": "img/18004250.jpg"},
+    
+    {"nombre": "CP BOMBA DE VACIO VINO INOX CL", "codigo": "18004158", "imagen": "img/18004158.jpg"}, 
+     
+    {"nombre": "CP COLLERIN CORTAGOTA CON LOGO CYT CL", "codigo": "18004130", "imagen": "img/18004130.jpg"},
+   
+     
+    {"nombre": "CP RUANA POLAR 1U CL", "codigo": "18003889", "imagen": "img/18003889.jpg"}, 
+    
+    
+     
+     
+    
+    {"nombre": "CP CORTACAPSULA S/ IMAN 1U CL", "codigo": "18003124", "imagen": "img/18003124.jpg"}, 
+    
+    {"nombre": "CP MANTA POLAR CYT 1UN", "codigo": "18002677", "imagen": "img/18002677.jpg"},
+    
+    {"nombre": "CP COPA TULIPA TOUR CYT 1U CL", "codigo": "18002526", "imagen": "img/18002526.jpg"},
+    {"nombre": "CYT TRIO BOLSO TERMICO", "codigo": "18002254", "imagen": "img/18002254.jpg"},
+    
+   
+    
+    {"nombre": "GORRO NIEVE CYT", "codigo": "18000531", "imagen": "img/18000531.jpg"},
+    {"nombre": "GORRO POLAR CASILLERO", "codigo": "18000530", "imagen": "img/18000530.jpg"},
+    {"nombre": "JOCKEY BORDADO CYT", "codigo": "18000423", "imagen": "img/18000423.jpg"},
+    
+    {"nombre": "CHILLINS", "codigo": "18000025", "imagen": "img/18000025.jpg"},
+    {"nombre": "GORRA DE CHEF", "codigo": "18000018", "imagen": "img/18000018.jpg"},
+    
+    {"nombre": "CP CJA MADERA CYT WINE SHOP 2BOT CL", "codigo": "13005809", "imagen": "img/13005809.jpg"},
+    
+    {"nombre": "CYT CDD BOLSO NOTEBOOK", "codigo": "18006691", "imagen": "img/18006691.jpg"},
+    {"nombre": "AIREADOR BELLOVINO", "codigo": "18010254", "imagen": "img/18010254.jpg"},
+    
+]
+
+ const contenedor = document.querySelector(".contenedor")
+ const boton = document.getElementById("boton")
+ const form = document.getElementById("input")
+ var card = document.getElementById("cards")
+ const menubtn = document.querySelector(".ContHamburguer")
+ const categoria = document.getElementById("categorias")
+ 
+
+
+    const filtrar = () =>{
+      contenedor.innerHTML = "";
+
+      const textoUsuario = form.value.toLowerCase();
+      for(let tarjeta of objeto){
+        let nombre = tarjeta.nombre.toLowerCase();
+
+        if(nombre.indexOf(textoUsuario) !== -1){
+          contenedor.innerHTML += 
+          `
+        <div class="card" id="cards">
+        
+        <div data-bs-toggle="modal" data-bs-target="#id${tarjeta.codigo}" class="divCard">
+            <img src="${tarjeta.imagen}" style= "width: 100%"  alt="">
+        </div>
+        <h4 id="titulo">${tarjeta.nombre}</h4>
+           <p class="card-text">${tarjeta.codigo}</p>
+    
+        
+        </div>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="id${tarjeta.codigo}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog  sss">
+        <img src="${tarjeta.imagen}" alt="" class="img-fluid rounded">
+        
+        </div>
+        </div>
+                `
+        }
+        
+      }
+    } 
+
+
+
+form.addEventListener("keyup", filtrar)
+
+
+filtrar()
+
+const total = document.getElementById("valorTotal");
+const totalObjeto = objeto.length;
+total.innerHTML = totalObjeto;
+
+
+
+
+function dark(){
+  const luna = document.querySelector(".fa-moon")
+  const sol = document.querySelector(".fa-sun")
+  
+
+  luna.classList.toggle("dark")
+  sol.classList.toggle("dark")
+
+  document.querySelector("body").classList.toggle("dark")
+  document.querySelector("#nombre").classList.toggle("oscuro")
+  
+  
+  form.classList.toggle("dark")
+ 
+}
+
+  
+  let menuOpen = false;
+
+  menubtn.addEventListener("click", ()=>{
+    document.querySelector(".ContHamburguer").classList.toggle("open")
+   
+
+  }); 
