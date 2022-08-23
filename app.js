35ece38c9f3100f82c016dbdf855a3b381715c60
vilchezfelipe <felipemarket.vilchez@outlook.com>
2022-08-22 21:07:17 -0400

    primer repositorio

---
+++ app.js
@@ -0,0 +1,246 @@
+const objeto = [
+  {"nombre": "CYT CDD MU PELOTA DE FOOTBALL VINTAGE", "codigo": "18011272", "imagen": "img/18011272.jpg", "precio": "18.990"},
+  {"nombre": "CP SET ACCESORIOS VINOS-GRANDE 1U CL", "codigo": "18003175", "imagen": "img/18003175.jpg", "precio": "18.990"},
+  {"nombre": "CP SET ACCESORIOS VINOS C/LOGO CYT 1U CL", "codigo": "18003084 ", "imagen": "img/18003084.jpg", "precio": "18.990"},
+  {"nombre": "CORAVIN TIMELESS THREE SL", "codigo": "18011433", "imagen": "img/18011433.jpg", "precio": "199.990"},
+  {"nombre": "CORAVIN - CAJA TRES CAPSULAS", "codigo": "18011434", "imagen": "img/18011434.jpg", "precio": "43.990"},
+  {"nombre": "CORAVIN - AIREADOR", "codigo": "18011436", "imagen": "img/18011436.jpg", "precio": "37.990"},
+  {"nombre": "CORAVIN- SET 3 AGUJAS", "codigo": "18011435", "imagen": "img/18011435.jpg", "precio": "87.990"},
+  {"nombre": "MANJAR 400 GR", "codigo": "18011404", "imagen": "img/18011404.jpg", "precio": "4.490"},
+  {"nombre": "ESCULTURA CORAZON DE PIRQUE", "codigo": "18011398", "imagen": "img/18011398.jpg", "precio": "69.990"},
+  {"nombre": "ESCULTURA ARAUCARIA", "codigo": "18011399", "imagen": "img/18011399.jpg", "precio": "99.990"},
+  {"nombre": "ESCULTURA HOJA DE PARRA", "codigo": "18011401", "imagen": "img/18011401.jpg", "precio": "99.990"},
+  {"nombre": "ESCULTURA PAJAROS", "codigo": "18011400", "imagen": "img/18011400.jpg", "precio": "99.990"},
+
+  {"nombre": "Set 4 vasos + Jarro Ceramica", "codigo": "18011423", "imagen": "img/18011423.jpg", "precio": "59.990"},
+  {"nombre": "Fuente Grande Ceramica", "codigo": "18011424", "imagen": "img/18011424.jpg", "precio": "44.990"},
+  {"nombre": "Fuente Mediana Ceramica", "codigo": "18011425", "imagen": "img/18011425.jpg", "precio": "39.990"},
+  {"nombre": "Azucarero Esgrafiado Ceramica", "codigo": "18011426", "imagen": "img/18011426.jpg", "precio": "35.990"},
+  {"nombre": "Tazones Esgrafiados Ceramica", "codigo": "18011427", "imagen": "img/18011427.jpg", "precio": "26.990"},
+  {"nombre": "Posillo Pajarito Ceramica", "codigo": "18011428", "imagen": "img/18011428.jpg", "precio": "26.990"},
+  {"nombre": "Pocillo Grande Ceramica", "codigo": "18011430", "imagen": "img/18011430.jpg", "precio": "17.990"},
+  {"nombre": "Pocillo Chico Ceramica", "codigo": "18011431", "imagen": "img/18011431.jpg", "precio": "13.990"},
+  {"nombre": "Fuente Plana Chica Ceramica", "codigo": "18011432", "imagen": "img/18011432.jpg", "precio": "6.990"},
+
+  {"nombre": "Tabla Paleta RdelV", "codigo": "18011395", "imagen": "img/18011395.jpg", "precio": "25.990"},
+  {"nombre": "Tabla Quesos RdelV", "codigo": "18011396", "imagen": "img/18011396.jpg", "precio": "28.990"},
+  {"nombre": "Tabla Pala RdelV", "codigo": "18011397", "imagen": "img/18011397.jpg", "precio": "69.990"},
+
+
+
+
+  {"nombre": "LIBRO AVES DE PIRQUE", "codigo": "18011405", "imagen": "img/18011405.jpg", "precio": "37.990"},
+  {"nombre": "MINI PECHERA RENO", "codigo": "18011290", "imagen": "img/18011290.jpg", "precio": "6.290"},
+  {"nombre": "CAPUCHON MONJE CAFE", "codigo": "18002222", "imagen": "img/18002222.jpg", "precio": ""},
+  {"nombre": "DECANTADOR RIEDEL CORNETO", "codigo": "18009824", "imagen": "img/18009824.jpg", "precio": "32.990"},
+  {"nombre": "CYT AIREADOR VINOX BASIC", "codigo": "18011325", "imagen": "img/18011325.jpg", "precio": "34.990"},
+  
+  {"nombre": "CYT CDD SOFTSHELL HOMBRE T/S", "codigo": "18006693", "imagen": "img/18006693.jpg", "precio": "54.990"},
+  {"nombre": "CDD PARKA NEGRA HOMBRE TALLA M", "codigo": "18009403", "imagen": "img/18009403.jpg", "precio": "59.990"},
+  {"nombre": "CDD POLERON DEVIL´S  TALLA L", "codigo": "18009468", "imagen": "img/18009468.jpg", "precio": "34.990"},
+  {"nombre": "MOCHTAL HOODY FLEECE PB", "codigo": "18009800", "imagen": "img/18009800.jpg", "precio": "34.990"},
+  {"nombre": "CYT DUHKO SOFTSWEATHER MEN", "codigo": "18009798", "imagen": "img/18009798.jpg", "precio": "39.990"},
+  {"nombre": "CYT CDD POLERA PIQUE HOMBRE T/L NEGRA 22", "codigo": "18011298", "imagen": "img/18011298.jpg", "precio": "13.990"},
+  {"nombre": "CYT CDD POLERA PIQUE HOMBRE T/M NEGRA 22", "codigo": "18011297", "imagen": "img/18011298.jpg", "precio": "13.990"},
+  {"nombre": "CYT CDD POLERA MU DOS DEVILS TALLA S", "codigo": "18010783", "imagen": "img/18010783.jpg", "precio": "14.990"},
+  {"nombre": "CYT CDD POLERA MU DOS DEVILS TALLA M", "codigo": "18010784", "imagen": "img/18010783.jpg", "precio": "14.990"},  
+  {"nombre": "CYT MANUTD POLERA PIQUE NEGRA HOMBRE T/M", "codigo": "18006600", "imagen": "img/18006600.jpg", "precio": "18.990"},
+  {"nombre": "PARKA MANGA LARGA ACOLCHADA CYT", "codigo": "18008971", "imagen": "img/18008971.jpg", "precio": "49.990"},
+    {"nombre": "PARKA SIN MANGA ACOLCHADA CYT", "codigo": "18008970", "imagen": "img/18008970.jpg", "precio": "34.990"},
+    {"nombre": "CYT MANUTD POLAR NEGRO HOMBRE T/L", "codigo": "18007261", "imagen": "img/18007261.jpg", "precio": "29.990"},
+
+    {"nombre": "CP POSAVASOS CUERINA C/ESCUDO CYT 1U CL", "codigo": "18003707", "imagen": "img/18003707.jpg", "precio": "10.990"},
+
+  {"nombre": "SOMBRERO DE PAJA", "codigo": "18010334", "imagen": "img/18010334.jpg", "precio": "39.990"},
+  {"nombre": "CP SOMBRERO CUERO DAMA CL", "codigo": "18006651", "imagen": "img/18006651.jpg", "precio": "35.990"},
+  {"nombre": "SOMBRERO CUERO HOMBRE", "codigo": "18000034", "imagen": "img/18000034.jpg", "precio": "34.990"},
+  {"nombre": "CP DELANTAL COCINA CUELLO RECTO CL", "codigo": "18004132", "imagen": "img/18004132.jpg", "precio": "10.990"},
+  
+  {"nombre": "PORTA BOTELLA CIRCULAR", "codigo": "18011289", "imagen": "img/18011289.jpg", "precio": "17.990"},
+  {"nombre": "CAJA MADERA 1 BOT CYT", "codigo": "13005808", "imagen": "img/13005808.jpg", "precio": "8.490"},
+  
+  {"nombre": "TABLA GUILLI", "codigo": "18010330", "imagen": "img/18010330.jpg", "precio": "37.990"},
+  {"nombre": "TABLA HACHI", "codigo": "18010331", "imagen": "img/18010331.jpg", "precio": "29.990"},
+  {"nombre": "TABLA TEKUN", "codigo": "18010329", "imagen": "img/18010329.jpg", "precio": "39.990"},
+  {"nombre": "TABLA QUESO + ACCESORIOS NP", "codigo": "18009863", "imagen": "img/18009863.jpg", "precio": "15.990"},
+  {"nombre": "TABLAS QUESO CERAMICA NP", "codigo": "18009817", "imagen": "img/18009817.jpg", "precio": "16.490"},
+  {"nombre": "DESCORCHADOR ECU ELECTRICO", "codigo": "18009738", "imagen": "img/18009738.jpg", "precio": "22.990"},
+  {"nombre": "DESCORCHADOR DIABLO", "codigo": "18010171", "imagen": "img/18010171.jpg", "precio": "4.990"},
+  {"nombre": "SACACORCHOS CYT MADERA", "codigo": "18009620", "imagen": "img/18009620.jpg", "precio": "6.990"},
+  {"nombre": "CYT CDD SACACORCHOS NEGRO", "codigo": "18005922", "imagen": "img/18005922.jpg", "precio": "4.990"},
+  {"nombre": "CP SACACORCHO CYT NUEVO 1U CL", "codigo": "18004103", "imagen": "img/18004103.jpg", "precio": "4.590"},
+  {"nombre": "CDD SACACORCHOS NUEVO", "codigo": "18001941", "imagen": "img/18001941.jpg", "precio": "4.990"},
+  {"nombre": "CYT SACACORCHO NEGRO C/LOGO", "codigo": "18000004", "imagen": "img/18000004.jpg", "precio": "3.990"},
+
+  {"nombre": "MUG CYT NARANJO", "codigo": "18009796", "imagen": "img/18009796.jpg", "precio": "6.490"},
+  {"nombre": "BOTELLA ALUMINIO NP", "codigo": "18009864", "imagen": "img/18009864.jpg", "precio": "6.490"},
+  {"nombre": "CYT DIABLO BOTELLA ACERO INOXIDABLE", "codigo": "18010989", "imagen": "img/18010989.jpg", "precio": "9.990"},
+
+    {"nombre": "DESMANCHADOR VINO", "codigo": "18010333", "imagen": "img/18010333.jpg", "precio": "8.990"},
+    {"nombre": "TAPON-ABRIDOR PARA ESPUMANTES PULLTEX", "codigo": "18010256", "imagen": "img/18010256.jpg", "precio": "11.990"},
+    {"nombre": "TAPON ANTIOX PULLTEX", "codigo": "18010255", "imagen": "img/18010255.jpg", "precio": "16.990"},
+    {"nombre": "TAPONES DE REPUESTO", "codigo": "18009435", "imagen": "img/18009435.jpg", "precio": "3.990"},
+    {"nombre": "TAPON VINO SILICONA", "codigo": "18009433", "imagen": "img/18009433.jpg", "precio": "4.490"},
+    
+    {"nombre": "CAPA DE AGUA", "codigo": "18010253", "imagen": "img/18010253.jpg", "precio": "2.990"},
+    
+    
+    {"nombre": "LLAVERO METAL RECT-REDONDO NP", "codigo": "18009861", "imagen": "img/18009861.jpg", "precio": "2.990"},
+    {"nombre": "LLAVERO METALICO CYT", "codigo": "18009023", "imagen": "img/18009023.jpg", "precio": ""},
+    {"nombre": "LLAVERO CUERO PEQUEÑO CYT", "codigo": "18003725", "imagen": "img/18003725.jpg", "precio": "3.790"},
+    {"nombre": "LLAVERO CUERO GRANDE CYT", "codigo": "18003512", "imagen": "img/18003512.jpg", "precio": "6.990"},
+    {"nombre": "CYT CDD LLAVERO", "codigo": "18003408", "imagen": "img/18003408.jpg", "precio": "3.590"},
+    {"nombre": "CAJA 1 BOT. BAMBU", "codigo": "18009745", "imagen": "img/18009745.jpg", "precio": "18.990"},
+    {"nombre": "CP CAJA ECOCUERO 2BOT+ACCESORIOS CL", "codigo": "18006678", "imagen": "img/18006678.jpg", "precio": "29.990"},
+    {"nombre": "CP CAJA ECOCUERO 1BOT+ACCESORIOS CL", "codigo": "18005578", "imagen": "img/18005578.jpg", "precio": "22.990"},
+  
+    {"nombre": "IMAN PORTAL CYT", "codigo": "18009050", "imagen": "img/18009050.jpg", "precio": "2.990"},
+    {"nombre": "IMAN BARRICA", "codigo": "18008799", "imagen": "img/18008799.jpg", "precio": "2.990"},
+
+    {"nombre": "CYT CDD HIELERA P21", "codigo": "18011176", "imagen": "img/18011176.jpg", "precio": "14.990"},
+    {"nombre": "HIELERA ACRILICA NP", "codigo": "18009818", "imagen": "img/18009818.jpg", "precio": "9.000"},
+    {"nombre": "CDD HIELERA TRANSPARENTE", "codigo": "18003948", "imagen": "img/18003948.jpg", "precio": ""},
+    {"nombre": "CDD HIELERA METALICA", "codigo": "18005173", "imagen": "img/18005173.jpg", "precio": "16.990"},
+    
+    
+    {"nombre": "SELFIE STICK NP", "codigo": "18009862", "imagen": "img/18009862.jpg", "precio": "5.990"},
+    {"nombre": "LAPIZ METAL NARANJO", "codigo": "18009815", "imagen": "img/18009815.jpg", "precio": "3.990"},
+    {"nombre": "LAPIZ NEGRO CYT", "codigo": "18009378", "imagen": "img/18009378.jpg", "precio": "2.990"},
+    {"nombre": "LAPIZ PLATEADO  CDD", "codigo": "18009660", "imagen": "img/18009660.jpg", "precio": "18009660"},
+    {"nombre": "LAPIZ MADERA CYT", "codigo": "18008798", "imagen": "img/18008798.jpg", "precio": "1.490"},
+    {"nombre": "BUFF CUELLO CYT NARANJO", "codigo": "18009797", "imagen": "img/18009797.jpg", "precio": "4.990"},
+    
+    {"nombre": "CDD STICKER ICONO DIABLO", "codigo": "18009412", "imagen": "img/18009412.jpg", "precio": "1.490"},
+    {"nombre": "CDD STICKER LOGO MARCA", "codigo": "18009411", "imagen": "img/18009411.jpg", "precio": "1.490"},
+    
+    {"nombre": "CYT CDD PENDRIVE NEGRO 16GB", "codigo": "18009244", "imagen": "img/18009244.jpg", "precio": "14.990"},
+    
+    {"nombre": "PIN ESCUDO CYT", "codigo": "18009025", "imagen": "img/18009025.jpg", "precio": "3.990"},
+    
+    
+    
+    
+    
+    {"nombre": "AIREADOR - DECANTADOR", "codigo": "18008517", "imagen": "img/18008517.jpg", "precio": "5.990"},
+    {"nombre": "DECANTADOR CENTELLINO", "codigo": "18008229", "imagen": "img/18008229.jpg", "precio": "38.990"},
+    {"nombre": "CP JOCKEY BORDADO CYT AZUL/BEIGE 1U CL", "codigo": "18001669", "imagen": "img/18001669.jpg", "precio": "7.990"},
+    {"nombre": "GORRO LEGIONARIO FILTRO UV CYT", "codigo": "18007982", "imagen": "img/18007982.jpg", "precio": "8.990"},
+    {"nombre": "CP SET MANGA ENFRIADORA 1U CL", "codigo": "18003659", "imagen": "img/18003659.jpg", "precio": "12.990"},
+    
+    {"nombre": "BOLSA PROTECTORA CYT", "codigo": "18009026", "imagen": "img/18009026.jpg", "precio": "2.990"},
+    {"nombre": "FUNDA NEO 2 BOT CYT", "codigo": "18009748", "imagen": "img/18009748.jpg", "precio": "8.990"},
+    {"nombre": "FUNDA NEO 1 BOT CYT", "codigo": "18004161", "imagen": "img/18004161.jpg", "precio": "5.690"}, 
+    
+    
+    {"nombre": "CP CORTAGOTAS 1UN CON LOGO CYT CL", "codigo": "18006233", "imagen": "img/18006233.jpg", "precio": "1.490"},
+    {"nombre": "CYT LIBRO CARMENERE ING", "codigo": "18006194", "imagen": "img/18006194.jpg", "precio": "16.990"}, 
+    {"nombre": "CYT LIBRO CARMENERE ESP", "codigo": "18006193", "imagen": "img/18006193.jpg", "precio": "16.990"}, 
+    
+    
+    {"nombre": "CDD PENDRIVE", "codigo": "18005523", "imagen": "img/18005523.jpg", "precio": "9.900"},
+     
+    {"nombre": "BOMBA DE VACIO VINO INOX", "codigo": "18004158", "imagen": "img/18004158.jpg", "precio": "7.990"}, 
+     
+    {"nombre": "COLLERIN CORTAGOTA", "codigo": "18004130", "imagen": "img/18004130.jpg", "precio": "1.990"},
+   
+    {"nombre": "CP CORTACAPSULA S/ IMAN 1U CL", "codigo": "18003124", "imagen": "img/18003124.jpg", "precio": "4.490"}, 
+    
+    {"nombre": "CP MANTA POLAR CYT 1UN", "codigo": "18002677", "imagen": "img/18002677.jpg", "precio": "26.990"},
+    
+    {"nombre": "CP COPA TULIPA TOUR CYT 1U CL", "codigo": "18002526", "imagen": "img/18002526.jpg", "precio": "2.490"},
+    {"nombre": "BOLSO NEGRO 3 BOT CYT", "codigo": "18002254", "imagen": "img/18002254.jpg", "precio": "13.990"},
+    
+   
+    {"nombre": "GORRO POLAR CASILLERO", "codigo": "18000530", "imagen": "img/18000530.jpg", "precio": "6.990"},
+    
+    {"nombre": "CAJA MADERA 2 BOT CYT", "codigo": "13005809", "imagen": "img/13005809.jpg", "precio": "9.990"},
+    
+    {"nombre": "CYT CDD BOLSO NOTEBOOK", "codigo": "18006691", "imagen": "img/18006691.jpg", "precio": "34.990"},
+    {"nombre": "AIREADOR BELLOVINO", "codigo": "18010254", "imagen": "img/18010254.jpg", "precio": "24.990"},
+    
+]
+
+
+
+ const contenedor = document.querySelector(".contenedor")
+ const boton = document.getElementById("boton")
+ const form = document.getElementById("input")
+ var card = document.getElementById("cards")
+ const menubtn = document.querySelector(".ContHamburguer")
+ const categoria = document.getElementById("categorias")
+ 
+
+
+    const filtrar = () =>{
+      contenedor.innerHTML = "";
+
+      const textoUsuario = form.value.toLowerCase();
+      for(let tarjeta of objeto){
+        let nombre = tarjeta.nombre.toLowerCase();
+
+        if(nombre.indexOf(textoUsuario) !== -1){
+          contenedor.innerHTML += 
+          `
+        <div class="card" id="cards">
+        
+        <div data-bs-toggle="modal" data-bs-target="#id${tarjeta.codigo}" class="divCard">
+            <img src="${tarjeta.imagen}" style= "width: 100%"  alt="">
+        </div>
+        <h4 id="titulo">${tarjeta.nombre}</h4>
+           <p class="card-text">${tarjeta.codigo}</p>
+           <p class="card-price">$${tarjeta.precio}</p>
+    
+        
+        </div>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="id${tarjeta.codigo}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog  sss">
+        <img src="${tarjeta.imagen}" alt="" class="img-fluid rounded">
+        
+        </div>
+        </div>
+                `
+        }
+        
+      }
+    } 
+
+
+
+form.addEventListener("keyup", filtrar)
+
+
+filtrar()
+
+const total = document.getElementById("valorTotal");
+const totalObjeto = objeto.length;
+total.innerHTML = totalObjeto;
+
+
+
+
+function dark(){
+  const luna = document.querySelector(".fa-moon")
+  const sol = document.querySelector(".fa-sun")
+  
+
+  luna.classList.toggle("dark")
+  sol.classList.toggle("dark")
+
+  document.querySelector("body").classList.toggle("dark")
+  document.querySelector("#nombre").classList.toggle("oscuro")
+  
+  
+  form.classList.toggle("dark")
+ 
+}
+
+  
+  let menuOpen = false;
+
+  menubtn.addEventListener("click", ()=>{
+    document.querySelector(".ContHamburguer").classList.toggle("open")
+   
+
+  }); 
